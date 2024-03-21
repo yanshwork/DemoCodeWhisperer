335c9078e331be7185291caa4aa9c8488acf31cc
Yan Shteckel <yanshwork@gmail.com>
2024-03-21 12:01:35 +0200

    update.

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Calculator {
+    // method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // method to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // method to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // method to divide two numbers
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ src/main/java/org/example/CalculatorTest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+//write a junit for Calculator class methods
